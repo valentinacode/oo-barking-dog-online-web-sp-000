6970d816bdf2de975842eec88ca373178d3378d5
Valentina E <tiyahelfrih@gmail.com>
2020-09-01 13:50:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,11 @@
-class Dog 
-  def name = Dog_name
-    @this_dogs_name = dog_name
-  end 
-
-def name
-  @this_dogs_name
+class Dog
+  def name=(dog_name)
+    this_dogs_name = dog_name
+  end
+ 
+  def name
+    this_dogs_name
+  end
 end 
 
 def bark 
